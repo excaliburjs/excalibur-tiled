c169f969856f7c0a095e7a016388ce81071b597a
Erik Onarheim <erik.onarheim@gmail.com>
2021-01-23 12:06:05 -0600

    Remove unneeded plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
-const webpack = require("webpack");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
  entry: {
@@ -39,12 +37,6 @@ module.exports = {
  },
  optimization: {
    minimize: true,
-   minimizer: [
-      new UglifyJsPlugin({
-         sourceMap: true,
-         include: /\.min\.js$/
-      })
-   ]
  },
  externals: {
     "excalibur": {
