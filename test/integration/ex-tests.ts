9fe2a89f9899783808051510369d0f2f5721bf75
Erik Onarheim <erik.onarheim@gmail.com>
2021-09-02 22:24:16 -0500

    Update to @excaliburjs/testing

---
+++ test/integration/ex-tests.ts
@@ -0,0 +1,43 @@
+import { expectLoaded, expectPage, test } from '@excaliburjs/testing'
+
+const selectMap = async (page, map: string) => {
+   const option = (await page.$x(
+      `//*[@id = "select-map"]/option[text() = "${map}"]`
+   ))[0];
+   const value = await (await option.getProperty('value')).jsonValue();
+   await page.select('#select-map', value);
+}
+
+const dashify = (string: string) => {
+   return string.replace(/\s/g, '-')
+}
+
+test('Tiled Tests', async (page) => {
+   let mapTypes = [
+      'tmx',
+      'tmx external tsx',
+      'tmx base64',
+      'tmx gzip',
+      'tmx zlib',
+      'tmx zstd',
+      'json',
+   ]
+   for (let map of mapTypes) {
+      await selectMap(page, map);
+      await expectLoaded();
+      await expectPage(map, `./test/integration/images/actual-${dashify(map)}.png`).toBe('./test/integration/images/expected-play.png');
+   }
+
+   const legacyMapTypes = [
+      'v1',
+      'v1 external tileset',
+      'v0 gzip',
+      'v0 zlib'
+   ];
+
+   for (let legacyMap of legacyMapTypes) {
+      await selectMap(page, legacyMap);
+      await expectLoaded();
+      await expectPage(legacyMap, `./test/integration/images/actual-${dashify(legacyMap)}.png`).toBe(`./test/integration/images/expected-play-${dashify(legacyMap)}.png`);
+   }
+});
