05b47688bb091dba8bb6b61cfae4afdc4c9ace3c
Pascal Garber <pascal@artandcode.studio>
2021-11-11 11:24:19 +0100

    test: Fix existing tests and add new z index layer tests

--- test/unit/tiled-map-resource.spec.ts
+++ test/unit/tiled-map-resource.spec.ts
@@ -12,11 +12,13 @@ describe('A Tiled Map Excalibur Resource', () => {
       
       expect(tiled.isLoaded()).toBe(true);
 
-      const map = tiled.getTileMap();
-      expect(map.cols).toBe(5);
-      expect(map.rows).toBe(5);
-      expect(map.cellWidth).toBe(16);
-      expect(map.cellHeight).toBe(16);
+      const layers = tiled.getTileMapLayers();
+      expect(layers).toHaveSize(1);
+
+      expect(layers[0].cols).toBe(5);
+      expect(layers[0].rows).toBe(5);
+      expect(layers[0].cellWidth).toBe(16);
+      expect(layers[0].cellHeight).toBe(16);
    });
 
    it('can load solid layers', async () => {
@@ -25,10 +27,44 @@ describe('A Tiled Map Excalibur Resource', () => {
 
       expect(tiled.isLoaded()).toBe(true);
 
-      const map = tiled.getTileMap();
+      const layers = tiled.getTileMapLayers();
+      expect(layers).toHaveSize(2);
+
       tiled.useSolidLayers();
       
-      expect(map.getCell(2, 2).solid).toBeTrue();
-      expect(map.getCell(0, 0).solid).toBeFalse();
+      expect(layers[1].getCell(2, 2).solid).toBeTrue();
+      expect(layers[1].getCell(0, 0).solid).toBeFalse();
+   });
+
+   it('can load layers with zindex', async () => {
+      const tiled = new TiledMapResource('base/test/unit/layer-zindex.tmx');
+      await tiled.load();
+
+      expect(tiled.isLoaded()).toBe(true);
+
+      const layers = tiled.getTileMapLayers();
+      expect(layers).toHaveSize(3);
+
+      tiled.useSolidLayers();
+      
+      expect(layers[0].z).toBe(-1);
+      expect(layers[1].z).toBe(0);
+      expect(layers[2].z).toBe(5);
+   });
+
+   it('can overwrite the base z-index start value', async () => {
+      const tiled = new TiledMapResource('base/test/unit/layer-zindex.tmx', undefined, -5);
+      await tiled.load();
+
+      expect(tiled.isLoaded()).toBe(true);
+
+      const layers = tiled.getTileMapLayers();
+      expect(layers).toHaveSize(3);
+
+      tiled.useSolidLayers();
+      
+      expect(layers[0].z).toBe(-5);
+      expect(layers[1].z).toBe(-4);
+      expect(layers[2].z).toBe(5);
    });
 });
