aa32629366a5c95289d566f045074274e9eb3dcb
Erik Onarheim <erik.onarheim@gmail.com>
2023-12-30 12:07:27 -0600

    add simple orthogonal

---
+++ example/external-fantasy.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="external-fantasy" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="tilemap_packed.png" width="192" height="176"/>
+ <tile id="93">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="coin" x="3.27273" y="11.6364">
+    <polygon points="0,0 -0.272727,-8.18182 1.54545,-9.90909 7.36364,-9.90909 9.72727,-7.72727 9.72727,0.545455 7.63636,2.27273 2,2.27273"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
