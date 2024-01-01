058d0451845214c226c2cfacc4b10030eece435d
Erik Onarheim <erik.onarheim@gmail.com>
2024-01-01 14:22:16 -0600

    update snapshots

--- test/integration/isometric.spec.ts
+++ test/integration/isometric.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('isometric matches', async ({ page }) => {
   await page.goto('http://localhost:8080/example/isometric/');
   await page.click('#excalibur-play');
-  await page.waitForTimeout(100);
+  await page.waitForTimeout(500);
   await expect(page).toHaveScreenshot();
 });
 
--- test/integration/orthogonal.spec.ts
+++ test/integration/orthogonal.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('orthogonal matches', async ({ page }) => {
   await page.goto('http://localhost:8080/example/orthogonal/');
   await page.click('#excalibur-play');
-  await page.waitForTimeout(100);
+  await page.waitForTimeout(500);
   await expect(page).toHaveScreenshot();
 });
 
