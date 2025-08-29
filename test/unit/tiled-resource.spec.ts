6642c4a989d168515b7fce914969e9b21878e2aa
Erik Onarheim <erik.onarheim@gmail.com>
2025-08-29 10:19:10 -0500

    fix tests (add method parity orthogonal and iso)

--- test/unit/tiled-resource.spec.ts
+++ test/unit/tiled-resource.spec.ts
@@ -399,11 +399,32 @@ describe('A Tiled map resource parser', () => {
 
 
    it('can get tile by layer and world pos in isometric infinite map', async () => {
-      const tiledMap = new TiledResource('/test/unit/tiled/tiled-resource-spec/orthogonal-infinite.tmx');
+      const tiledMap = new TiledResource('/test/unit/tiled/tiled-resource-spec/isometric-infinite.tmx');
 
       await tiledMap.load();
 
       const tile = tiledMap.getTileByPoint('ground', vec(8, 8));
+
+      expect(tile).not.toBeNull();
+   });
+  
+   it('can get tile by layer and coord in orthogonal infinite map', async () => {
+      const tiledMap = new TiledResource('/test/unit/tiled/tiled-resource-spec/orthogonal-infinite.tmx');
+
+      await tiledMap.load();
+
+      const tile = tiledMap.getTileByCoordinate('ground', 1, 1);
+
+      expect(tile).not.toBeNull();
+   });
+
+
+   it('can get tile by layer and coord in isometric infinite map', async () => {
+      const tiledMap = new TiledResource('/test/unit/tiled/tiled-resource-spec/isometric-infinite.tmx');
+
+      await tiledMap.load();
+
+      const tile = tiledMap.getTileByCoordinate('ground', 1, 1);
 
       expect(tile).not.toBeNull();
    });
